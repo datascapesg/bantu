e2954d63ce977eb6c355666e68253909886495c6
LoneRifle <LoneRifle@users.noreply.github.com>
2021-05-22 17:42:19 +0800

    Initial commit

---
+++ postprocess.js
@@ -0,0 +1,22 @@
+import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.10/mod.ts' 
+
+const toName = ({ name }) => name
+
+
+// Read the downloaded_filename JSON
+const { data } = await readJSON(Deno.args[0])
+
+// Rework data, cleaning up tags and categories to be simple string arrays
+const cleanedData = data.map(d => {
+  const { tags, categories, skills } = d
+  return {
+    ...d,
+    tags: tags.map(toName),
+    skills: skills.map(toName),
+    categories: categories.map(toName),
+  }
+})
+
+// Write a new JSON file with our filtered data
+await writeJSON(Deno.env.get('POSTPROCESS_FILENAME'), cleanedData) // create a new JSON file with just the Bitcoin price
+console.log('Done.')
